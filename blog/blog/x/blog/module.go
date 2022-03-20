9c42614b681bc4418ec86585530626e33fc92a1d
Vatsa Pr <vatsamail@gmail.com>
2022-03-20 14:56:40 -0700

    after updating module.go with RegisterGRPCGatewayRoutes and import context

--- blog/blog/x/blog/module.go
+++ blog/blog/x/blog/module.go
@@ -1,6 +1,7 @@
 package blog
 
 import (
+    "context" //added by vatsa
 	"encoding/json"
 	"fmt"
 	// this line is used by starport scaffolding # 1
@@ -77,7 +78,7 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	// this line is used by starport scaffolding # 2
+    types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx)) // added by vatsa
 }
 
 // GetTxCmd returns the capability module's root tx command.
