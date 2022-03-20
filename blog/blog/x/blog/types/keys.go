00d903f5adb51e8d6af1d053262218fa87c066b8
Vatsa Pr <vatsamail@gmail.com>
2022-03-20 11:58:02 -0700

    added ID in tx.proto; updated CreatePost function in msg_server_create_post.go; created post.proto; touched post.go; updated keys.go with Post- prefixes

--- blog/blog/x/blog/types/keys.go
+++ blog/blog/x/blog/types/keys.go
@@ -15,6 +15,10 @@ const (
 
 	// MemStoreKey defines the in-memory store key
 	MemStoreKey = "mem_blog"
+
+    // Keep track of the index of posts  
+  PostKey      = "Post-value-" // added by vatsa
+  PostCountKey = "Post-count-" // added by vatsa
 )
 
 func KeyPrefix(p string) []byte {
